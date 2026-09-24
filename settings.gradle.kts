ed85d5f4b86cb76d25bad3dcdf2146c6b9a94f18
minhhieu271 <138102177+minhhieu271@users.noreply.github.com>
2026-09-25 03:01:38 +0700

    Remove duplicate version catalog registration so Gradle can use gradle/libs.versions.toml without the invalid catalog definition error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,11 +29,6 @@ dependencyResolutionManagement {
         }
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
